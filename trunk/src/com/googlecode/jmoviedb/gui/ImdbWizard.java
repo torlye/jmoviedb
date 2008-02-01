d2e3bf064f899e2286185a5ade56924e2a3f7e78
torarnelye <torarnelye@0499a085-b239-0410-910f-3374d81603cc>
2008-02-01 23:30:24 +0000

    More imdb mass update work

--- trunk/src/com/googlecode/jmoviedb/gui/ImdbWizard.java
+++ trunk/src/com/googlecode/jmoviedb/gui/ImdbWizard.java
@@ -1,19 +1,30 @@
 package com.googlecode.jmoviedb.gui;
 
-import java.awt.Checkbox;
-
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.wizard.ProgressMonitorPart;
 import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.FontMetrics;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
-public class ImdbWizard extends Wizard{
+import com.googlecode.jmoviedb.gui.action.MassUpdateAction;
+
+public class ImdbWizard extends Wizard {
+	
+	// Minimum dialog width (in dialog units) (value from class TitleAreaDialog)
+	private static final int MIN_DIALOG_WIDTH = 350;
+	
+	public Label label0;
+	public Composite c0;
+	int labelWidth;
 	
 	public ImdbWizard() {
 		super();
@@ -22,9 +33,20 @@ public class ImdbWizard extends Wizard{
 	}
 	
 	public void addPages() {
-		addPage(new WizardPage1());
-		addPage(new WizardPage2());
-		addPage(new WizardPage3());
+		Composite c = new Composite(getShell(), SWT.NONE);
+		GC gc = new GC(c);
+		gc.setFont(c.getFont());
+		FontMetrics fontMetrics = gc.getFontMetrics();
+		gc.dispose();
+		c.dispose();
+		
+		labelWidth = Dialog.convertHorizontalDLUsToPixels(fontMetrics, MIN_DIALOG_WIDTH);
+		System.out.println("labelWidth "+labelWidth);
+
+		WizardPage wiz2 = new WizardPage2();
+		WizardPage wiz3 = new WizardPage3();
+		addPage(wiz2);
+		addPage(wiz3);
 	}
 
 	public boolean canFinish() {
@@ -38,22 +60,21 @@ public class ImdbWizard extends Wizard{
 		return false;
 	}
 	
-	private class WizardPage1 extends WizardPage {
-		public WizardPage1() {
-			super("IMDB download introduction");
-			setTitle(getName());
-			setPageComplete(true);
-		}
-		public void createControl(Composite parent) {
-			Composite c =  new Composite(parent, SWT.NULL);
-			GridLayout gl = new GridLayout();
-			c.setLayout(gl);
-			Label l1 = new Label(c, SWT.NONE);
-			l1.setText("Some text here");
-			
-			setControl(c);
-		}
-	}
+//	private class WizardPage1 extends WizardPage {
+//		public WizardPage1() {
+//			super("IMDb download");
+//			setTitle(getName());
+//			setPageComplete(true);
+//		}
+//		public void createControl(Composite parent) {
+//			c0 =  new Composite(parent, SWT.BORDER);
+//			c0.setLayout(new GridLayout());
+//			label0 = new Label(c0, SWT.BORDER);
+//			label0.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL|GridData.FILL_BOTH));
+//			
+//			setControl(c0);
+//		}
+//	}
 	
 	private class WizardPage2 extends WizardPage {
 		public WizardPage2() {
@@ -62,33 +83,43 @@ public class ImdbWizard extends Wizard{
 			setPageComplete(true);
 		}
 		public void createControl(Composite parent) {
-			Composite c =  new Composite(parent, SWT.NULL);
-			GridLayout gl = new GridLayout();
-			gl.numColumns = 2;
+			Composite c =  new Composite(parent, SWT.BORDER);
+			GridLayout gl = new GridLayout(2, false);
 			c.setLayout(gl);
-			
-			GridData gd1 = new GridData();
-			gd1.grabExcessHorizontalSpace = true;
-			gd1.grabExcessVerticalSpace = true;
-			gd1.horizontalSpan=2;
-			Label l1 = new Label(c, SWT.NONE);
-			l1.setText("Can I Has Cheezburger?");
-			l1.setLayoutData(gd1);
 			
 			Button check1 = new Button(c, SWT.CHECK);
 			Label l2 = new Label(c, SWT.NONE);
-			l2.setText("Check1");
-			Label l4 = new Label(c, SWT.NONE);
-			l4.setText("Check1 details");
+			l2.setText("Keep titles");
+			Label l4 = new Label(c, SWT.WRAP);
+			l4.setText("If a movie's title is different from the title used at IMDb, your custom title will be copied to the custom title field while the new title will be stored in the title field. This prevents a movie like \"Seven Samurai\" from being renamed to \"Shichinin no samurai\" (IMDb uses the movie's title in the original language). If the box is unchecked, all titles are overwritten with IMDb's titles.");
+			GridData gd1 = new GridData(labelWidth, SWT.DEFAULT);
+			gd1.grabExcessVerticalSpace = true;
+			gd1.horizontalSpan = 2;
+			gd1.verticalAlignment = SWT.TOP;
 			l4.setLayoutData(gd1);
 			
 			
 			Button check2 = new Button(c, SWT.CHECK);
 			Label l3 = new Label(c, SWT.NONE);
-			l3.setText("Check2");
-			Label l5 = new Label(c, SWT.NONE);
-			l5.setText("Check1 details");
-			l5.setLayoutData(gd1);
+			l3.setText("Skip movies with no IMDb URL");
+			Label l5 = new Label(c, SWT.WRAP);
+			l5.setText("If this box is unckecked, JMoviedb will try to search for blah blah blah....");
+			GridData gd2 = new GridData(labelWidth, SWT.DEFAULT);
+			gd2.grabExcessVerticalSpace = true;
+			gd2.horizontalSpan = 2;
+			gd2.verticalAlignment = SWT.TOP;
+			l5.setLayoutData(gd2);
+			
+			Button check3 = new Button(c, SWT.CHECK);
+			Label l6 = new Label(c, SWT.NONE);
+			l6.setText("Skip movies that have been updated before");
+			Label l1 = new Label(c, SWT.WRAP);
+			l1.setText("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas lectus. Phasellus eget justo ac dui egestas dictum. Curabitur lacinia mi a magna. Nulla elementum, purus at tempus pellentesque, ante quam tempor nunc, at egestas velit justo ut urna.");
+			GridData gd3 = new GridData(labelWidth, SWT.DEFAULT);
+			gd3.grabExcessVerticalSpace = true;
+			gd3.horizontalSpan = 2;
+			gd3.verticalAlignment = SWT.TOP;
+			l1.setLayoutData(gd3);
 			
 			setControl(c);
 		}
@@ -108,6 +139,7 @@ public class ImdbWizard extends Wizard{
 			l1.setText("lol!");
 			
 			setControl(c);
+			System.out.println(parent.computeSize(SWT.DEFAULT, SWT.DEFAULT).x);
 		}
 	}
 }
--- trunk/src/com/googlecode/jmoviedb/gui/action/MassUpdateAction.java
+++ trunk/src/com/googlecode/jmoviedb/gui/action/MassUpdateAction.java
@@ -1,20 +1,35 @@
 package com.googlecode.jmoviedb.gui.action;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IPageChangedListener;
 import org.eclipse.jface.dialogs.IPageChangingListener;
 import org.eclipse.jface.dialogs.PageChangedEvent;
 import org.eclipse.jface.dialogs.PageChangingEvent;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Layout;
+import org.eclipse.swt.widgets.Shell;
 
 import com.googlecode.jmoviedb.gui.ImdbWizard;
 import com.googlecode.jmoviedb.gui.MainWindow;
 
 public class MassUpdateAction extends Action {
 	
-	private WizardDialog wd;
+	private ImdbWizardDialog wd;
+	private boolean hasRun;
+	private static final int MIN_DIALOG_WIDTH = 350;
 	
 	public MassUpdateAction() {
 		setText("Update all movies");
@@ -22,19 +37,62 @@ public class MassUpdateAction extends Action {
 		setImageDescriptor(null);
 	}
 	
+	ImdbWizard wizard = new ImdbWizard();
 	public void run() {
-		ImdbWizard wizard = new ImdbWizard();
-		wd = new WizardDialog(MainWindow.getMainWindow().getShell(), wizard);
+		hasRun = false;
+		wd = new ImdbWizardDialog(MainWindow.getMainWindow().getShell(), wizard);
+//		wd.configureShell()
+//		wd.setPageSize(450, SWT.DEFAULT);
+//				wd.setPageSize(500, SWT.DEFAULT);
 		wd.addPageChangedListener(new IPageChangedListener() {
 			public void pageChanged(PageChangedEvent evt) {
-				// TODO Auto-generated method stub
-				System.out.println(((IWizardPage)(evt.getSelectedPage())).getName());
+				for(IWizardPage page : wizard.getPages())
+//					page.getControl().setSize(page.getControl().computeSize(500, SWT.DEFAULT));
+				
+//				wd.setShellWidth(500);
+//				wd.getShell().setSize(500, wd.getShell().getSize().y);
+				try {
+					if(wd.getCurrentPage().getTitle().equals("Downloading...") && hasRun==false) {
+						wd.run(true, true, new Foo());
+						hasRun = true;
+					}
+//					System.out.println("shell bounds "+wizard.getShell().getBounds());
+				} catch (InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		});
+//		wd.initializeDialogUnits(wd.get);
+//		System.out.println("page size blir "+wd.convertHorizontalDLUsToPixels(MIN_DIALOG_WIDTH));
+//		wd.setPageSize(400, SWT.DEFAULT);
 		wd.open();
-		
-		wizard.dispose();
 		wd = null;
 	}
+	
+	
+	public class ImdbWizardDialog extends WizardDialog {
+		private Composite pageContainer;
+		
+		public ImdbWizardDialog(Shell parentShell, IWizard newWizard) {
+			super(parentShell, newWizard);
+			setShellStyle(SWT.DIALOG_TRIM|SWT.APPLICATION_MODAL|getDefaultOrientation());
+		}
+				
+		public Composite getPageContainer() {
+			return pageContainer;
+		}
+	}
+	
 
+	private class Foo implements IRunnableWithProgress {
+		public void run(IProgressMonitor arg0)
+				throws InvocationTargetException, InterruptedException {
+			arg0.beginTask("Running the amazing infinite loop", IProgressMonitor.UNKNOWN);
+			while(!arg0.isCanceled()) {}
+		}
+	}
 }
