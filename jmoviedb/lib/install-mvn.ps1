ac7682e563678c1c522592bd76ff2eeb374cea3c
torlye <>
2022-06-07 19:05:59 +0200

    Upgrade Eclipse SDK

--- jmoviedb/lib/install-mvn.ps1
+++ jmoviedb/lib/install-mvn.ps1
@@ -1,13 +1,12 @@
 mvn install:install-file -Dfile="lib/BrowserLauncher2-1_3.jar" -DgroupId="edu.stanford.ejalbert" -DartifactId=BrowserLauncher2 -Dversion="1.3" -Dpackaging=jar
 
-mvn install:install-file -Dfile="lib/org.eclipse.jface_3.10.0.v20140604-0740.jar" -DgroupId="org.eclipse" -DartifactId=jface -Dversion="3.10.0.v20140604-0740" -Dpackaging=jar
+mvn install:install-file -Dfile="lib/org.eclipse.jface_3.25.0.v20220218-1636.jar" -DgroupId="org.eclipse" -DartifactId=jface -Dversion="3.25.0.v20220218-1636" -Dpackaging=jar
 
-mvn install:install-file -Dfile="lib/org.eclipse.core.commands_3.6.100.v20140528-1422.jar" -DgroupId="org.eclipse.core" -DartifactId=commands -Dversion="3.6.100.v20140528-1422" -Dpackaging=jar
+mvn install:install-file -Dfile="lib/org.eclipse.core.commands_3.10.100.v20210722-1426.jar" -DgroupId="org.eclipse.core" -DartifactId=commands -Dversion="3.10.100.v20210722-1426" -Dpackaging=jar
 
-mvn install:install-file -Dfile="lib/org.eclipse.equinox.common_3.6.200.v20130402-1505.jar" -DgroupId="org.eclipse.equinox" -DartifactId=common -Dversion="3.6.200.v20130402-1505" -Dpackaging=jar
+mvn install:install-file -Dfile="lib/org.eclipse.equinox.common_3.16.0.v20220211-2322.jar" -DgroupId="org.eclipse.equinox" -DartifactId=common -Dversion="3.16.0.v20220211-2322" -Dpackaging=jar
 
-#mvn install:install-file -Dfile="lib/swt.jar" -DgroupId="org.eclipse" -DartifactId=swt -Dversion="3.659" -Dpackaging=jar
-mvn install:install-file -Dfile="lib/org.eclipse.swt.gtk.linux.x86_64_3.6.2.v3659b.jar" -DgroupId="org.eclipse" -DartifactId=swt -Dversion="3.659" -Dpackaging=jar
+mvn install:install-file -Dfile="lib/org.eclipse.swt.gtk.linux.x86_64_3.119.0.v20220223-1102.jar" -DgroupId="org.eclipse" -DartifactId=swt -Dversion="3.119.0.v20220223-1102" -Dpackaging=jar
 
 mvn install:install-file -Dfile="lib/KTable_2.2.0.jar" -DgroupId="de.kupzog" -DartifactId=ktable -Dversion="2.2.0" -Dpackaging=jar
 
